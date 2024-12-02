4552586069fb256e2f93e8a50bbaa2757527385e
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-02 13:18:38 +0700

    aturan main (naming convention) nest.js

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
-import { Controller, Get, Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return "Hello World!";
-  }
-}
 
-@Module({
-  controllers: [AppController],
-})
-class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
