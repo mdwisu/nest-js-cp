f9bd6455667d3cb9a84c0f070a1f9868ee380be5
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-02 17:08:30 +0700

    mendefinisikan route sebuah controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get('/')
+  listTasks() {
+    return 'tasks';
+  }
+
+  @Post('/')
+  createTask() {
+    return 'create task';
+  }
+
+  @Get('/:id')
+  getTask() {
+    return 'get task';
+  }
+}
